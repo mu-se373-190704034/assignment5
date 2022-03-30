3b7baf905507fccc2a7a1fe26698db7d4d5fa4dd
efsaceren <100808407+efsaceren@users.noreply.github.com>
2022-03-30 11:49:39 +0300

    Initial commit

--- lib/settings.dart
+++ lib/settings.dart
@@ -1,21 +1,23 @@
 import 'package:assignment5/main.dart';
 import 'package:flutter/material.dart';
 
-class AboutPage extends StatelessWidget {
+class SettingsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) => Scaffold(
     appBar: AppBar(
-      title: Text('ABOUT'),
+      title: Text('SETTINGS'),
       centerTitle: true,
       backgroundColor: Color.fromARGB(255, 0, 0, 0),
     ),
     body: Center(
+
       child: ElevatedButton(
         child: Text("GO TO FIRST PAGE", style: TextStyle(fontSize: 20)),
         onPressed: () => Navigator.push(
           context,
           MaterialPageRoute(builder: (context) => myApp()),
         ),
+
       ),
     ),
   );
